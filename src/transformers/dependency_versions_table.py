01df6501bcdcde2a6affcba679679718d77d3cae
Quentin Lhoest <lhoest.q@gmail.com>
2022-10-12 19:13:37 +0200

    datasets@main

--- src/transformers/dependency_versions_table.py
+++ src/transformers/dependency_versions_table.py
@@ -10,7 +10,7 @@ deps = {
     "codecarbon": "codecarbon==1.2.0",
     "cookiecutter": "cookiecutter==1.7.3",
     "dataclasses": "dataclasses",
-    "datasets": "datasets!=2.5.0",
+    "datasets": "datasets @ git+https://github.com/huggingface/datasets@main#egg=datasets",
     "decord": "decord==0.6.0",
     "deepspeed": "deepspeed>=0.8.3",
     "dill": "dill<0.3.5",
